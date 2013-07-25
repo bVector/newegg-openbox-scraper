0e964fcc75f024fe1479b332022268511c15b95d
Tony Narlock <tony@git-pull.com>
2013-07-25 13:21:50 +0800

    Update gitignore scraper

--- scraper.py
+++ scraper.py
@@ -6,25 +6,41 @@ import requests
 URL = 'http://www.newegg.com'
 URL = 'http://www.newegg.com/Open-Box/Store?Type=OPENBOX'
 
-r = requests.get(URL).text
-soup = BeautifulSoup(r)
 
-products = soup.select(".productCells .unit_gallery .wrap_inner")
+
 #products_soup = [BeautifulSoup(p) for p in products]
 #print list(products_soup)[0]
 
 #print products[0]
+class Product(object):
 
+    def __init__(self, URL):
+        r = requests.get(URL).text
+        soup = BeautifulSoup(r)
+        self.products_soup = soup.select(".productCells .unit_gallery .wrap_inner")
 
-def get_desc(tags):
-    for tag in tags:
-        yield tag.find('div', class_='wrap_description') \
-            .find('span', class_='descText') \
-            .text \
-            .rsplit('Open Box: ', 1)[1]
+    def get_desc(self, tags):
+        for tag in tags:
+            yield tag.find('div', class_='wrap_description') \
+                .find('span', class_='descText') \
+                .text \
+                .rsplit('Open Box: ', 1)[1]
 
-descs = get_desc(products)
-print list(descs)
+    def get_products(self):
+        return self.get_desc(self.products_soup)
+
+    def __str__(self):
+        return '\n'.join(self.get_products())
+
+    def __unicode__(self):
+        return ''.join(self.get_products)
+
+#descs = get_desc(products)
+#print list(descs)
+
+s = Product(URL)
+#print list(s.get_products())
+print s
 #desc = descblock.find('span', class_='descText').text
 
 #print soup.prettify()
