ff46b20429763db52080c0a574c7e848712978ee
Tony Narlock <tony@git-pull.com>
2013-07-27 21:48:35 -0500

    generators, classes for getting categories

--- scraper.py
+++ scraper.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup
 
 import grequests
 import requests
+import tornado.httpclient
+import tornado.ioloop
 from pprint import pprint
 
 URL = 'http://www.newegg.com'
@@ -34,40 +36,94 @@ def get_products(URL):
     return soup.select(".itemCell")
 
 
-def get_product_categories(URL):
-    r = requests.get(URL).text
-    soup = BeautifulSoup(r)
-    soup_list = soup.find("div", class_='blaNavigation') \
-                    .find('dl', class_='categoryList') \
-                    .find_all('dd')
-    import re
-    for c in soup_list:
-        quantity = int(
-            re.match(
-                '\((\d+)\)',
-                c.find('span', class_='grey')
-                .get_text(strip=True)
-            )
-            .group(1)
-        )
 
-        yield {
-            'text': next(c.a.stripped_strings),
-            'href': c.a['href'],
-            'quantity': quantity
-        }
+
+import gevent
+from tornado import gen
+def fetch_page(URL):
+    http = tornado.httpclient.AsyncHTTPClient()
+    fetch = http.fetch(URL, get_product_categories)
+    return gen.Task(fetch)
+
+#souped = (grequests.get(c['href']) for c in get_product_categories(URL))
+class Category(object):
+
+    def __init__(self, **kwargs):
+        self.products = None
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+    def __str__(self):
+        return unicode(self).encode('utf-8')
+
+    def __unicode__(self):
+        return self.text
+
+
+class Categories(object):
+    def __init__(self, URL):
+        self.URL = URL
+
+    def fetch(self):
+        import requests
+        r = requests.get(self.URL)
+        return r.text
+
+    def __iter__(self):
+        return iter(self.parse())
+
+    def parse(self):
+        #r = requests.get(URL).text
+        self.categories = list()
+        soup = BeautifulSoup(self.fetch())
+        soup_list = soup.find("div", class_='blaNavigation') \
+                        .find('dl', class_='categoryList') \
+                        .find_all('dd')
+        import re
+        for c in soup_list:
+            quantity = int(
+                re.match(
+                    '\((\d+)\)',
+                    c.find('span', class_='grey')
+                    .get_text(strip=True)
+                )
+                .group(1)
+            )
+
+            self.categories.append(Category(**{
+                'text': next(c.a.stripped_strings),
+                'href': c.a['href'],
+                'quantity': quantity
+            }))
+
+        return self.categories
+
+
 
 
 products = list()
+cats = Categories(URL)
 
-for category in get_product_categories(URL):
-    products.extend(
-        [Product(product) for product in get_products(category['href'])]
-    )
+def add_products_to_categories(categories):
+    for c in categories:
+        items = [Product(product) for product in get_products(c.href)]
+        c.products = items
+        yield c
 
-for product in products:
+cats = add_products_to_categories(cats)
+
+for c in cats:
+    print("'%s' has products attribute, type %s, with %s items" %\
+            (c.text, type(c.products),\
+            len(c.products)))
     try:
-        pprint(str(product))
-    except UnicodeEncodeError:
-        import chardet
-        pprint(chardet.detect(product))
+        print(c.text)
+        for product in c.products:
+            try:
+                pprint(str(product))
+                pass
+            except UnicodeEncodeError:
+                import chardet
+                pprint(chardet.detect(product))
+    except TypeError:
+        print "no items for %s" % c.text
